abcfb1d180ae1661a28b52e82ee5972f29c6a47c
SebastianBarra <barra20sebastian02p19@gmail.com>
2024-04-02 22:08:19 -0400

    solving observations

--- src/main/java/dataReader/DataReader.java
+++ src/main/java/dataReader/DataReader.java
@@ -1,14 +1,11 @@
-package org.example.fuzzy_matcher.util;
+package dataReader;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.objects.Music;
 import org.example.objects.MusicList;
-import org.example.objects.User;
 import org.example.objects.UserList;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 public class DataReader {
 
@@ -29,14 +26,4 @@ public class DataReader {
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        UserList userList = readUsersFromJson("src/main/resources/users.json");
-        List<User> users = userList.getUsers();//lista de usuarios que contiene la informacion de cada usuario
-        System.out.println(users.size());
-
-        MusicList musicList = readMusicListFromJson("src/main/resources/tracks.json");
-        List<Music> tracks = musicList.getTracks(); // lista de tracks que contiene la informacion de cada track
-        System.out.println(tracks.size());
-
-    }
 }
